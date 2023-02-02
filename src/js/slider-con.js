bba44db7e9cdc176ef9d110d4b53cfb0bc3debae
VVervolk <vervolk1234@gmail.com>
2023-02-02 23:21:59 +0200

    Revert "Merge pull request #15 from VVervolk/feature/restorant-details"
    This reverts commit 8bdf5d245aa2c75ce6874e9aa8eb8b60bc8493eb, reversing
    changes made to 2b783a24739bafb3494d236e13a8478c45a4b2e4.

--- src/js/slider-con.js
+++ src/js/slider-con.js
@@ -2,7 +2,6 @@ function swiperSlider() {
   const miniSlider = document.querySelectorAll('[data-slider="mini-slider"]');
   const Slider = document.querySelectorAll('[data-slider="slider"]');
   const heroSlider = document.querySelectorAll('[data-slider="hero-slider"]');
-  const restSlider = document.querySelectorAll('[data-slider="rest-slider"]');
   if (miniSlider) {
     miniSlider.forEach(slider => {
       //
@@ -218,79 +217,6 @@ function swiperSlider() {
         //   //   spaceBetween: 40,
         //   // },
         // },
-      });
-    });
-  }
-  if (restSlider) {
-    miniSlider.forEach(slider => {
-      //
-      let arrowLeft = slider.querySelector('.swiper-button-prev');
-      let arrowRight = slider.querySelector('.swiper-button-next');
-      //
-
-      // let pagination = slider.querySelector('.swiper-pagination');
-      swiper = new Swiper(slider.querySelector('.swiper'), {
-        speed: 1500,
-        // loop: true,
-        //centeredSlides: true,
-        centeredSlides: true,
-        // autoplay: {
-        //     delay: 3000,
-        //     disableOnInteraction: false,
-        // },
-
-        slidesPerView: 3,
-        spaceBetween: 20,
-        // pagination: {
-        //     el: pagination,
-        //     clickable: true,
-        //     renderBullet: function (index, className) {
-        //         return '<li class="' + className + '"></li>';
-        //     },
-        // },
-        //
-        navigation: {
-          nextEl: arrowRight,
-          prevEl: arrowLeft,
-        },
-        //
-        on: {
-          transitionStart: function () {
-            let previousIndex = this.previousIndex;
-            let previousSlide =
-              slider.getElementsByClassName('swiper-slide')[previousIndex];
-            if (previousSlide) {
-              setTimeout(function () {
-                previousSlide.classList.remove('is-play');
-              }, 1000);
-            }
-          },
-          transitionEnd: function () {
-            let activeIndex = this.activeIndex;
-            let activeSlide =
-              slider.getElementsByClassName('swiper-slide')[activeIndex];
-            activeSlide.classList.add('is-play');
-          },
-        },
-        breakpoints: {
-          // when window width is >= 0px
-          0: {
-            slidesPerView: 'auto',
-            // centeredSlides: true,
-            spaceBetween: 25,
-          },
-          // when window width is >= 1280px
-          1028: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-            centeredSlides: false,
-          },
-          // when window width is >= 640px
-          // 1280: {
-          //   slidesPerView: 3,
-          //   spaceBetween: 40,
-          // },
-        },
       });
     });
   }
